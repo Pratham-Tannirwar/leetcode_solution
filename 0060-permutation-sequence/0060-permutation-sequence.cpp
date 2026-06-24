22b71cf2e5bcd7adbd18631adb2430098dd9974e
Pratham Tannirwar <tannirwarpratham@gmail.com>
2026-06-24 10:15:15 +0530

    Time: 58 ms (11.72%), Space: 7.9 MB (94.52%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s="";
+        for(int i=1;i<=n;i++){
+            s.push_back('0'+i);
+        }
+        string ch=s;
+        for(int i=2;i<=k;i++){
+
+            next_permutation(ch.begin(),ch.end());
+        }
+        return ch;
+    }
+};
