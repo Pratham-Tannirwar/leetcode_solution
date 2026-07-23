e511ccfb943263d1bf9fbb61c9db7af4b5b6a437
Pratham Tannirwar <tannirwarpratham@gmail.com>
2026-07-24 04:27:32 +0530

    Time: 0 ms (100%), Space: 198.8 MB (59.63%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         size_t n = nums.size();
-        return 1 << (bit_width(n) - 3 / (n + 1));
+        int m = n;
+        
+        m |= m >> 1;
+        m |= m >> 2;
+        m |= m >> 4;
+        m |= m >> 8;
+        m |= m >> 16;
+        
+        return (m + 1) >> (3 / (n + 1));
     }
 };
